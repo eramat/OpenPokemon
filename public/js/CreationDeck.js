237a40ce391c03c6a4668218486a5375de5d36ca
nathan pecqueux <pecqueux.nathan@hotmail.fr>
2016-06-14 15:48:04 +0200

    Page GestionDeck, onglets finis

--- public/js/CreationDeck.js
+++ public/js/CreationDeck.js
@@ -2,8 +2,10 @@
  * Created by Nathan on 14/06/2016.
  */
 
+var anc_onglet = 'Tous';
+change_onglet(anc_onglet);
+
 function change_onglet(name) {
-    
     document.getElementById('onglet_'+anc_onglet).className = 'onglet_0 onglet';
     document.getElementById('onglet_'+name).className = 'onglet_1 onglet';
     document.getElementById('contenu_onglet_'+anc_onglet).style.display = 'none';
@@ -11,5 +13,7 @@ function change_onglet(name) {
     anc_onglet = name;
 }
 
-var anc_onglet = 'Tous';
-change_onglet(anc_onglet);
+function change_apercu(num) {
+    var a = 25;
+    return a;
+}
